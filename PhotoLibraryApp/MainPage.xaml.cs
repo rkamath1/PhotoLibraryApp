2ef05cd2389105d4210c6bf5bf739f04069f9e92
rkamath1 <33260647+rkamath1@users.noreply.github.com>
2019-08-13 13:07:52 -0700

    Photo Library App Initial

--- PhotoLibraryApp/MainPage.xaml.cs
+++ PhotoLibraryApp/MainPage.xaml.cs
@@ -12,6 +12,13 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
+using Windows.System;
+using Windows.UI.Xaml.Shapes;
+using System.Diagnostics;
+using Windows.Storage.Pickers;
+using Windows.Storage.Provider;
+using System.Collections.ObjectModel;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +29,49 @@ namespace PhotoLibraryApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private ObservableCollection<string> current;
         public MainPage()
         {
             this.InitializeComponent();
+            current = new ObservableCollection<string>();
+            current.Add("C://Users/Ranja/Pictures/2017-08/IMG_4138.JPG");
+            current.Add("C://Users/Ranja/Pictures/2017-08/IMG_4139.JPG");
+            current.Add("C://Users/Ranja/Pictures/2017-08/IMG_4140.JPG");
+            this.DataContext = current;
+        }
+
+        private async void Import_Photos_Button_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            {
+                FileOpenPicker openPicker = new FileOpenPicker();
+                openPicker.ViewMode = PickerViewMode.Thumbnail;
+                openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+                openPicker.FileTypeFilter.Add(".jpg");
+                openPicker.FileTypeFilter.Add(".jpeg");
+                openPicker.FileTypeFilter.Add(".png");
+
+                StorageFile file = await openPicker.PickSingleFileAsync();
+
+
+                if (file != null)
+                {
+                    // Application now has read/write access to the picked file
+                    this.textBlock.Text = "Picked photo: " + file.Name;
+
+                }
+                else
+                {
+                    this.textBlock.Text = "Operation cancelled.";
+                }
+
+            }
+
+
+        }
+
+        private void Album_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(AlbumPage));
         }
     }
 }
